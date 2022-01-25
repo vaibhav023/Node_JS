808adb3b52c132c86572834a8ccdadc7bff9ee2a
vaibhav023 <91330995+vaibhav023@users.noreply.github.com>
2022-01-25 18:39:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ if (!isProduction) {
 if(isProduction){
   mongoose.connect(process.env.MONGODB_URI);
 } else {
-  mongoose.connect('mongodb://172.31.84.5:27017,172.31.83.125:27017,172.31.88.89:27017/conduit');
+  mongoose.connect('mongodb://localhost/conduit');
   mongoose.set('debug', true);
 }
 
